5ad4d9ed5d9271af7712226ca893e577cfbfe0aa
marrasluca <lmarras10@gmail.com>
2025-02-26 14:59:21 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+
+[
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+
+function App() {
+
+  const[filteredMovies, setFilteredMovies ] = use
+ 
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
