10bf14b210fdda32305a688652001d125e492cfb
marrasluca <lmarras10@gmail.com>
2025-02-26 15:32:41 +0100

    end

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,39 @@ const initialMovies = [
 ]
 
 
+
+
 function App() {
 
   const[filteredMovies, setFilteredMovies ] = useState(initialMovies);
   const[genre, setGenre] = useState('');
+
+  useEffect( () => {
+    if( genre !== '' ){
+      const selectedMovies = initialMovies.filter ( (movie) => movie.genre === genre);
+      setFilteredMovies( selectedMovies )
+    } else {
+      setFilteredMovies( initialMovies )
+    }
+  }, [ genre ])
  
 
   return (
     <>
     <div className="container">
       <h1>Lista films</h1>
+
+      <div className="mb-5">
+        <select  className="form-control" name="" value={genre} onChange={(e) => setGenre(e.target.value)}>
+          <option value="">Tutti</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+      </div>
+
+
       <ul className="list-group">
         {
         filteredMovies.map( (movie) => {
