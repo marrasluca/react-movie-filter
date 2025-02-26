e803a7f44deee997f959a0a31a7cac7243bce67c
marrasluca <lmarras10@gmail.com>
2025-02-26 15:15:25 +0100

    add: lista film

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import { useState, useEffect } from "react"
 
-[
-  { title: 'Inception', genre: 'Fantascienza' },
-  { title: 'Il Padrino', genre: 'Thriller' },
-  { title: 'Titanic', genre: 'Romantico' },
-  { title: 'Batman', genre: 'Azione' },
-  { title: 'Interstellar', genre: 'Fantascienza' },
-  { title: 'Pulp Fiction', genre: 'Thriller' },
+const initialMovies = [
+  { id: 1, title: 'Inception', genre: 'Fantascienza' },
+  { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+  { id: 3, title: 'Titanic', genre: 'Romantico' },
+  { id: 4, title: 'Batman', genre: 'Azione' },
+  { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+  { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
 ]
 
 
 function App() {
 
-  const[filteredMovies, setFilteredMovies ] = use
+  const[filteredMovies, setFilteredMovies ] = useState(initialMovies);
+  const[genre, setGenre] = useState('');
  
 
   return (
     <>
+    <div className="container">
+      <h1>Lista films</h1>
+      <ul className="list-group">
+        {
+        filteredMovies.map( (movie) => {
+            return(
+              <li key={movie.id} className="list-group-item">
+                {movie.title} -  {movie.genre}
+              </li>
+            )
+        })
+        }
+      </ul>
+    </div>
     </>
   )
 }
